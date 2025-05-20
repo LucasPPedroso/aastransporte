39f50b3be1b50eaf81732112d6988c245fa2ee17
lucasguimepa <lucas.pedroso@guimepa.com.br>
2025-05-20 17:14:14 -0300

    Update site title and description for AAS Transportes

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "AAS Transportes";
+export const SITE_DESCRIPTION = "Bem-vindo ao AAS Transportes, sua empresa de transporte de cargas e encomendas. Aqui você encontra soluções rápidas e seguras para suas necessidades de transporte. Nossa equipe está pronta para atender você com eficiência e qualidade. Entre em contato conosco e descubra como podemos ajudar a sua empresa a crescer!";
