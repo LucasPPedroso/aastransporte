1887d5b77a3a9857d87398afd65107864e54328c
Lucas Pedroso <lucaspedroso819@gmail.com>
2025-05-20 23:48:04 -0300

    Refactor site metadata and remove unused About page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "AAS Transportes";
-export const SITE_DESCRIPTION = "Bem-vindo ao AAS Transportes, sua empresa de transporte de cargas e encomendas. Aqui você encontra soluções rápidas e seguras para suas necessidades de transporte. Nossa equipe está pronta para atender você com eficiência e qualidade. Entre em contato conosco e descubra como podemos ajudar a sua empresa a crescer!";
+export const SITE_TITLE = 'AAS Transportes - Soluções em Transporte e Logística';
+export const SITE_DESCRIPTION = 'Especializada em transporte de cargas, a AAS Transportes oferece soluções completas em logística, com frota própria, segurança e pontualidade em todo o Brasil.';
