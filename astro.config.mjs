c23d1174607794d996f0f8767332b30d6c595ccb
Lucas Pedroso <lucaspedroso819@gmail.com>
2025-05-21 21:54:34 -0300

    Update Astro configuration to include MDX integration and change site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+// @ts-check
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://www.aastransportes.com.br',
-  integrations: [sitemap()],
+  site: "https://example.com",
+  integrations: [mdx(), sitemap()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
 });
